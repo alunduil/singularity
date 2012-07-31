fe1d0b630588185afad3a2d5cb92820117e914a7
Alex Brandt <alunduil@alunduil.com>
2012-07-31 18:13:11 -0500

    Adding stubs for xen communication.

--- singularity/communicators/__init__.py
+++ singularity/communicators/__init__.py
@@ -3,9 +3,36 @@
 # singularity is freely distributable under the terms of an MIT-style license.
 # See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-Communicator = None
+import logging
+import os
 
-if os.access(os.path.join(os.path.sep, "proc", "xen", "capabilities"), os.R_OK):
-    from singularity.communicators.xen import XenCommunicator
-    Communicator = XenCommunicator
+logger = logging.getLogger(__name__) # pylint: disable=C0103
+
+class Communicator(object):
+    @classmethod
+    def communicator(cls, *args, **kwargs):
+        """Factory returns communication mechanism for the current hypervisor.
+
+        ### Description
+
+        Uses a facter style determination of the hypervisor we are running under
+        and returns the appropriate communication translator for that bus.
+
+        We check that the communicator found is a subclass of Communicator only
+        to ensure we correctly handle known errors from the API provided.  Also
+        since this is not dynamically pluggable it acts as a check that nothing
+        blatant is missing upon release.
+
+        """
+
+        communicator = None
+
+        if os.access(os.path.join(os.path.sep, "proc", "xen", "capabilities"), os.R_OK): # pylint: disable=C0301
+            from singularity.communicators.xen import XenCommunicator
+            communicator = XenCommunicator(*args, **kwargs)
+
+        if not isinstance(communicator, Communicator):
+            communicator = None
+
+        return communicator
 
---
+++ singularity/communicators/xen.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2012 by Alex Brandt <alunduil@alunduil.com>
+#
+# singularity is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import logging
+
+from singularity.communicators import Communicator
+
+logger = logging.getLogger(__name__) # pylint: disable=C0103
+
+class XenCommunicator(Communicator):
+    pass
+
