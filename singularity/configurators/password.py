3cedb6e9c8ecd260f5d361b29003f5f905f956c2
Alex Brandt <alunduil@alunduil.com>
2012-08-07 21:37:48 -0500

    Adding password and resolver configurators.

---
+++ singularity/configurators/password.py
@@ -0,0 +1,65 @@
+# Copyright (C) 2012 by Alex Brandt <alunduil@alunduil.com>
+#
+# singularity is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import logging
+
+from singularity.configurators import SingularityConfigurator
+
+logger = logging.getLogger(__name__) # pylint: disable=C0103
+
+class PasswordConfigurator(SingularityConfigurator):
+    @property
+    def functions(self):
+        """Fulfills password function."""
+
+        return ["password"]
+
+    def runnable(self, configuration):
+        """True if configurator can run on this system and in this context.
+
+        ### Arguments
+
+        Argument      | Description
+        --------      | -----------
+        configuration | Configuration items to be applied (dict)
+
+        ### Description
+
+        We should be able to run if the following conditions are true:
+        * Running as root
+        * Can find the passwd command
+
+        """
+
+        if pwd.getpwuid(os.getuid())[0] != "root":
+            return False
+
+        if subprocess.call("which passwd", shell = True) != 0:
+            return False
+
+        if "password" not in configuration:
+            return False
+
+        return True
+
+    def content(self, configuration):
+        """Generated content of this configurator as a dictionary.
+        
+        ### Arguments
+
+        Argument      | Description
+        --------      | -----------
+        configuration | Configuration settings to be applied by this configurator (dict)
+
+        ### Description
+
+        Runs the password commond on the password provided.
+
+        """
+
+        subprocess.call("passwd {0}".format(configuration["password"]), shell = True)
+
+        return { "": "" }
+
---
+++ singularity/configurators/resolvers.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2012 by Alex Brandt <alunduil@alunduil.com>
+#
+# singularity is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import logging
+
+from singularity.configurators import SingularityConfigurator
+
+logger = logging.getLogger(__name__) # pylint: disable=C0103
+
+class ResolversConfigurator(SingularityConfigurator):
+    @property
+    def resolvconf_path(self):
+        return os.path.join(os.path.sep, "etc", "resolv.conf")
+
+    @property
+    def functions(self):
+        """Fulfills resolvers function."""
+
+        return "resolvers"
+
+    def runnable(self, configuration):
+        """True if configurator can run on this system and in this context.
+
+        ### Arguments
+
+        Argument      | Description
+        --------      | -----------
+        configuration | Configuration items to be applied (dict)
+
+        ### Description
+
+        We should be able to run if the following conditions are true:
+        * An /etc/resolv.conf file is writable
+        * We recieved the resolvers
+        * The resolver entries don't already exist
+
+        """
+
+        if not os.access(self.resolvconf_path, "w"):
+            return False
+
+        if "resolvers" not in configuration:
+            return False
+
+        existing_resolvers = set()
+        with open(self.resolvconf_path, "r") as resolvconf:
+            for line in resolvconf:
+                match = re.search(r"nameserver.*(?P<ip>(?:\d{1,3}\.){3}\d{1,3})", line)
+                if not match:
+                    continue
+                existing_resolvers.add(match.group("ip"))
+        if existing_resolvers >= set([ resolver[0] for resolver in configuration["resolvers"] ])
+            return False
+
+        return True
+
+    def content(self, configuration):
+        """Generated content of this configurator as a dictionary.
+        
+        ### Arguments
+
+        Argument      | Description
+        --------      | -----------
+        configuration | Configuration settings to be applied by this configurator (dict)
+
+        ### Description
+
+        Merge the resolver information from the configuration into the existing
+        resolvers information.
+
+        """
+
+        lines = []
+
+        if "hostname" in configuration:
+            domain = ".".join(configuration["hostname"].split(".")[-2:])
+
+        # TODO Improve the merge performed here.
+        with open(self.resolvconf_path, "r") as resolvconf:
+            lines.extend([ line.strip() for line in resolvconf.readlines() ])
+
+            if "domain" not in "".join(lines):
+                lines.append("domain {0}".format(domain))
+
+            if "search" not in "".join(lines):
+                lines.append("search {0}".format(domain))
+
+            for resolver in configuration["resolvers"]:
+                if resolver[0] not in "".join(lines):
+                    lines.append("nameserver {0}".format(resolver[0])
+
+        return { self.resolvconf_path: lines }
+
