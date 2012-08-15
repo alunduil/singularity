b3c1647a75b1b68341b80e9cac7ae433fb2a1456
Alex Brandt <alunduil@alunduil.com>
2012-08-15 18:28:58 -0500

    Removed regex for a substring match.

--- singularity/communicators/xencommunicator.py
+++ singularity/communicators/xencommunicator.py
@@ -231,7 +231,7 @@ class XenCommunicator(Communicator):
     @property
     def files(self):
         fd_path = os.path.join(os.path.sep, "proc", "self", "fd")
-        return [ fd for fd in os.listdir(fd_path) if re.search(r"xen", os.path.realpath(os.path.join(fd_path, fd))) ]
+        return [ fd for fd in os.listdir(fd_path) if os.path.realpath(os.path.join(fd_path, fd)).find("xen") != -1 ]
 
     def receive(self):
         """Recieve message from hypervisor and package for upstream consumption
