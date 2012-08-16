4175177bc98f7dfeb33b77d30f0cc4f7b2100132
Alex Brandt <alunduil@alunduil.com>
2012-08-15 22:30:34 -0500

    Still playing with logging formats.

--- singularity/application.py
+++ singularity/application.py
@@ -57,8 +57,8 @@ class SingularityApplication(object): # pylint: disable=R0903
         dhl.setLevel(logging.DEBUG)
         nhl.setLevel(logging.INFO)
 
-        dhl.setFormatter(logging.Formatter("\ufeff %(levelname)s => %(name)s: %(pathname)s:%(lineno)d in %(funcName)s: %(message)s")) # pylint: disable=C0301
-        nhl.setFormatter(logging.Formatter("\ufeff %(levelname)s => %(name)s: %(message)s")) # pylint: disable=C0301
+        dhl.setFormatter(logging.Formatter("{0}: %(levelname)s => %(name)s: %(pathname)s:%(lineno)d in %(funcName)s: %(message)s".format(sys.argv[0]))) # pylint: disable=C0301
+        nhl.setFormatter(logging.Formatter("{0}: %(levelname)s => %(name)s: %(message)s".format(sys.argv[0]))) # pylint: disable=C0301
 
         class LevelFilter(logging.Filter):
             def __init__(self, level, *args, **kwargs):
