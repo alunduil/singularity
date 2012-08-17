62a137df87a7649063431f26cef2c3cd5080726e
Alex Brandt <alunduil@alunduil.com>
2012-08-17 16:31:57 -0500

    Troubleshooting log format.

--- singularity/application.py
+++ singularity/application.py
@@ -57,8 +57,11 @@ class SingularityApplication(object): # pylint: disable=R0903
         dhl.setLevel(logging.DEBUG)
         nhl.setLevel(logging.INFO)
 
-        dhl.setFormatter(logging.Formatter("1 %(asctime)s - %(processName)s %(process)d - - \ufeff %(levelname)s => %(name)s: %(pathname)s:%(lineno)d in %(funcName)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
-        nhl.setFormatter(logging.Formatter("1 %(asctime)s - %(processName)s %(process)d - - \ufeff %(levelname)s => %(name)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
+        #dhl.setFormatter(logging.Formatter("1 %(asctime)s - %(processName)s %(process)d - - \ufeff %(levelname)s => %(name)s: %(pathname)s:%(lineno)d in %(funcName)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
+        #nhl.setFormatter(logging.Formatter("1 %(asctime)s - %(processName)s %(process)d - - \ufeff %(levelname)s => %(name)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
+        
+        dhl.setFormatter(logging.Formatter("1 %(asctime)s singularity: %(levelname)s => %(name)s: %(pathname)s:%(lineno)d in %(funcName)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
+        nhl.setFormatter(logging.Formatter("1 %(asctime)s singularity: %(levelname)s => %(name)s: %(message)s", '%Y-%m-%dT%H:%M:%S.%f%z')) # pylint: disable=C0301
 
         class LevelFilter(logging.Filter):
             def __init__(self, level, *args, **kwargs):
