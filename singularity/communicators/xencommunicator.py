0e4505bf4e4665010d401d8b728dde911735910f
Alex Brandt <alunduil@alunduil.com>
2012-08-20 14:48:12 -0500

    Using the wrong logic for queue waits.

--- singularity/communicators/xencommunicator.py
+++ singularity/communicators/xencommunicator.py
@@ -279,7 +279,7 @@ class XenCommunicator(Communicator):
         path = None
         message = None
 
-        while message is None and path is None:
+        while message is None or path is None:
             logger.debug("Current message at path, %s: %s", path, message)
             path, message = self._queue.get(timeout = sys.maxint)
 
