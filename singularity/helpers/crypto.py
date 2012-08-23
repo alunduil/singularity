13829841c5a9d944d89aecf8bc5c95fe0c967690
Alex Brandt <alunduil@alunduil.com>
2012-08-22 21:43:50 -0500

    Fixing the AES import once again.

--- singularity/helpers/crypto.py
+++ singularity/helpers/crypto.py
@@ -7,7 +7,7 @@ import logging
 import binascii
 import os
 import hashlib
-import Crypto.Cipher
+import Crypto.Cipher.AES
 import base64
 
 logger = logging.getLogger(__name__) # pylint: disable=C0103
