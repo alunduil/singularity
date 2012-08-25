07429d8e01ba4944cc393fd375e85c48ad90483e
Alex Brandt <alunduil@alunduil.com>
2012-08-24 20:36:55 -0500

    Fixing the default configurators allowed.

--- singularity/parameters/arguments.py
+++ singularity/parameters/arguments.py
@@ -67,7 +67,7 @@ class SingularityArguments(object):
             apply_parser.add_argument(*options["args"], **options["kwargs"]) # pylint: disable=W0142,C0301
 
         apply_parser.add_argument("action", metavar = "ACTION", nargs = "+",
-                choices = [ "all", "network", "hosts", "resolvers", "reboot", "password", "file", "update" ], # pylint: disable=C0301
+                choices = [ "all", "network", "hosts", "hostname", "resolvers", "password", "file", "update" ], # pylint: disable=C0301
                 help = "Specifies the action(s) to apply to the system.")
 
         daemon_parser = subparsers.add_parser('daemon', parents = [common],
